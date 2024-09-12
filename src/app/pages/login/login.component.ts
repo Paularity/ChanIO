230349289479ca4e665f368c2c9d5f94567237e7
paularity <christiandecembrana1@gmail.com>
2024-09-12 16:56:22 +0800

    prime ng and layout

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -11,18 +11,24 @@ import { Router } from '@angular/router';
   imports: [FormsModule, CommonModule],
   standalone: true,
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   username: string = '';
   password: string = '';
   errorMessage: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
 
+  ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['']);
+    }
+  }
+
   login(): void {
     if (this.authService.login(this.username, this.password)) {
       // Redirect or update the UI on successful login
       console.log('Login successful');
-      this.router.navigate(['home']);
+      this.router.navigate(['']);
     } else {
       this.errorMessage = 'Invalid username or password';
     }
