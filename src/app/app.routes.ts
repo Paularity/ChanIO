7a57fd32bf1ce2c0e701ff9326ecf525f3229630
paularity <christiandecembrana1@gmail.com>
2024-09-12 16:28:34 +0800

    initial login and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/errors/not-found/not-found.component';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: '404', component: NotFoundComponent },
+    { path: '**', redirectTo: '/404' } 
+];
