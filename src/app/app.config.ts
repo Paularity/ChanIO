b4fd1a3743cac0d5d0b829686ecc3a89f9ca2b5e
paularity <christiandecembrana1@gmail.com>
2024-09-12 23:50:00 +0800

    update env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { APP_BASE_HREF } from '@angular/common';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    { provide: APP_BASE_HREF, useValue: environment.baseUrl },
+  ],
 };
