e416315facf3af4e1ec091009dfbff80529a8ac8
paularity <christiandecembrana1@gmail.com>
2024-09-13 00:33:53 +0800

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+    { provide: APP_BASE_HREF, useValue: '/ChanIO' },
   ],
 };
